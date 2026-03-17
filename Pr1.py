10dd03fcf1bd11c725b881826040e68da89d5464
Alina2135 <alina2002066@gmail.com>
2026-03-17 22:18:35 +0200

    Виправила баг

--- Pr1.py
+++ Pr1.py
@@ -27,7 +27,9 @@ root.title("Пробний Калькулятор")
 
 # UI/UX помилка 1: низький контраст кнопок (світлий текст на світлому фоні)
 root.configure(bg="white")
+
 # Коментар з main
+
 entry = tk.Entry(root, width=25, font=("Arial", 14))
 entry.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
 
